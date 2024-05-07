1d52933ee890fd1ac092f6b88e6d6420950b172a
Abhishek Verma <vermabhishek657@gmail.com>
2024-05-07 14:38:04 +0530

    Update Db.js

--- Db.js
+++ Db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // Connection URI
-const uri = 'mongodb://127.0.0.1:27017/SocialX';
+const uri = 'mongodb+srv://Abhishek:Abhi8898@cluster0.ddqlz.mongodb.net/?retryWrites=true&w=majority&appName=SocialX';
 
 // Connect to MongoDB
  mongoose.connect(uri, { 
